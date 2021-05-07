0415fcab17e72f229e8001087ca411d908df1834
BermanBai <50157088+BermanBai@users.noreply.github.com>
2021-05-08 00:26:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+
+import Footer from "./components/Footer.js";
+
+import Navbar from "./components/Navbar.js";
+
+import Home from "./components/Home.js";
+
+import Login from "./components/Login.js";
+
+import Quiz from "./components/Quiz.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter>
+      
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+      
+                <Route path="/login" component={Login} />
+      
+                <Route path="/quiz/:id" component={Quiz} />
+            </Switch>
+            <Footer />
+        </HashRouter>
+    );
 }
 
 export default App;
